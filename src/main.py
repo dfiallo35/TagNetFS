6e60b8ef0ab1168a08fa5f9bb94cb07800f63ec9
Dennis Fiallo <dfiallo35@gmail.com>
2023-04-21 13:12:42 -0400

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from argparse import ArgumentParser
+
+
+# NOTE Roles: Almacenar datos, enrutar pedidos, consultar el sistema, etc
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description='Tag Based File System')
