9ed73b2b628ec136143f8514ff47619b0ec76445
Dennis Fiallo <dfiallo35@gmail.com>
2023-04-29 16:22:25 -0400

    db update

--- src/database/database.py
+++ src/database/database.py
@@ -5,8 +5,12 @@ from sqlalchemy.orm import declarative_base
 SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+    future=True,
+    echo=True,
 )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
