6f545fec02455e09846fa562f197d2ec738d4e98
Dennis Fiallo <dfiallo35@gmail.com>
2023-04-29 00:43:45 -0400

    db pydantic schemas

---
+++ src/database/schemas.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel
+from typing import Union
+
+
+class TagBase(BaseModel):
+    name: str
+
+class TagCreate(TagBase):
+    pass
+
+class Tag(TagBase):
+    name: str
+
+    class Config:
+        orm_mode = True
+
+
+class FileBase(BaseModel):
+    direction: str
+
+class FileCreate(FileBase):
+    pass
+
+class File(FileBase):
+    id: int
+    direction: str
+
+
+    class Config:
+        orm_mode = True
+
