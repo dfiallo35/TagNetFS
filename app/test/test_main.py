bd9d4da164baa67afc3331f97e9cddede92f8e32
Dennis Fiallo <dfiallo35@gmail.com>
2023-05-10 11:32:27 -0400

    test endpoints

--- app/test/test_main.py
+++ app/test/test_main.py
@@ -21,17 +21,67 @@ def test_add():
     response = client.post('/add', json=data)
     
     assert response.status_code == 200
-    assert response.json() == ["file1", "file2", "file3"]
+    assert response.json() == {"message": "success"}
+
 
 
 # test list
 def test_list():
-    data = [
-        "tag1"
-    ]
-    response = client.get('/list', params=data)
+    data = {
+        "tag_query": [
+            "tag1"
+        ]
+    }
+    response = client.get('/files/', params=data)
     
     assert response.status_code == 200
     assert response.json() == [
-        {'id': 1, 'name': 'file1'}
+        {'id': 1, 'name': 'file1'},
+        {'id': 2, 'name': 'file2'},
+        {'id': 3, 'name': 'file3'}
     ]
+
+
+# test add_tags
+def test_add_tags():
+    data = {
+        "tag_query": [
+            "tag1"
+        ],
+        "tag_list": [
+            "tag3"
+        ]
+    }
+    response = client.post('/add_tags', json=data)
+    
+    assert response.status_code == 200
+    assert response.json() == {"message": "success"}
+
+
+# test delete_tags
+def test_delete_tags():
+    data = {
+        "tag_query": [
+            "tag1"
+        ],
+        "tag_list": [
+            "tag3"
+        ]
+    }
+    response = client.delete('/delete_tags', params=data)
+    
+    assert response.status_code == 200
+    assert response.json() == {"message": "success"}
+
+
+# test delete
+def test_delete():
+    data = {
+        "tag_query": [
+            "tag1"
+        ]
+    }
+    response = client.delete('/delete', params=data)
+    
+    assert response.status_code == 200
+    assert response.json() == {"message": "success"}
